0cfdb0820c2e23fff76f9b8be68529f8442017c9
Gregory Coomber <gr.coomber@gmail.com>
2020-12-19 19:12:01 -0800

    Changed the game logic into a function call

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
-// Set the scores for player and computer to 0
-
 function showScore () {
 	alert(`Current score - User: ${playerScore}.  Computer: ${computerScore}`)
 }
 
+function compareResults (user, computer) {
+	
+	if (user === computer) {
+		alert(`You threw ${user} and the computer threw ${computer}.  Tie!`)
+		showScore()
+
+	} else if (user === 'rock' && computer === 'scissors') {
+
+		alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+		playerScore += 1
+		showScore()
+
+	} else if (user === 'rock' && computer === 'paper') {
+		
+		alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+		computerScore += 1
+		showScore()
+
+	} else if (user === 'paper' && computer === 'rock') {
+
+		alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+		playerScore += 1
+		showScore()
+
+	} else if (user === 'paper' && computer === 'scissors') {
+
+		alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+		computerScore += 1
+		showScore()
+
+	} else if (user === 'scissors' && computer === 'paper') {
+
+		alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+		playerScore += 1
+		showScore()
+
+	} else if (user === 'scissors' && computer === 'rock') {
+
+		alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+		computerScore += 1
+		showScore()
+
+	}
+
+}
+
+// Set the scores for player and computer to 0
+
 let playerScore = 0
 let computerScore = 0
-let choices = ["rock", "paper", "scissors"]
+
+// Create an array of the acceptable choices.
+
+const choices = ["rock", "paper", "scissors"]
 
 // Take an input from the user
-// Ensure the input is either rock, paper or scissors, case insensitive
-
+// Ensure the input is either rock, paper, or scissors, case insensitive
 
 while (playerScore < 3 && computerScore < 3) {
 
@@ -26,58 +74,20 @@ while (playerScore < 3 && computerScore < 3) {
 		}
 	}
 
+	// Convert the choice to lowercase to compare and tell the user what they picked.
+
 	playerChoice = playerChoice.toLowerCase()
-	alert(`You chose ${playerChoice}.`)
 
 	// Make the computer pick a choice between rock, paper and scissors at random.
 
 	let computerChoice = choices[Math.floor(Math.random() * choices.length)]
-
+	
 	// Compare the choices, and decide if the user or computer wins, and add one to the score of the winner
 
-	if (playerChoice === computerChoice) {
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  Tie!`)
-		showScore()
-
-	} else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You win!`)
-		playerScore += 1
-		showScore()
-
-	} else if (playerChoice === 'rock' && computerChoice === 'paper') {
-		
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You lost!`)
-		computerScore += 1
-		showScore()
-
-	} else if (playerChoice === 'paper' && computerChoice === 'rock') {
-
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You win!`)
-		playerScore += 1
-		showScore()
-
-	} else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You lost!`)
-		computerScore += 1
-		showScore()
-
-	} else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You win!`)
-		playerScore += 1
-		showScore()
-
-	} else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-
-		alert(`You threw ${playerChoice} and the computer threw ${computerChoice}.  You lost!`)
-		computerScore += 1
-		showScore()
-
-	}
+	compareResults(playerChoice, computerChoice)
 
 }
+
 
 alert(`Final score is User: ${playerScore} - Computer: ${computerScore}`)
 
