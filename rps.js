0b5c19d07d5fd0330bc9ade9d0587a8e91e8578a
Gregory Coomber <gr.coomber@gmail.com>
2020-12-24 13:16:50 -0800

    Copied script.js -> rps.js to keep backup.  script.js rewritten to work with buttons on index.html instead of through prompt.

---
+++ rps.js
@@ -0,0 +1,93 @@
+function showScore () {
+	alert(`Current score - User: ${playerScore}.  Computer: ${computerScore}`)
+}
+
+function compareResults (user, computer) {
+	
+	if (user === computer) {
+		alert(`You threw ${user} and the computer threw ${computer}.  Tie!`)
+		showScore()
+
+	} else if (user === 'rock') {
+		if (computer === 'scissors') {
+			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+			playerScore ++
+			showScore()
+		} else {
+			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+			computerScore ++
+			showScore()
+		}
+	
+	} else if (user === 'paper') {
+		if (computer === 'rock') {
+			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+			playerScore ++
+			showScore()
+		} else {
+			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+			computerScore ++
+			showScore()
+		}
+
+	} else if (user === 'scissors') {
+		if (computer === 'paper') {
+			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+			playerScore ++
+			showScore()
+		} else {
+			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
+			computerScore ++
+			showScore()
+		}
+	}
+}
+
+// Set the scores for player and computer to 0
+
+let playerScore = 0
+let computerScore = 0
+
+// Create an array of the acceptable choices.
+
+const choices = ["rock", "paper", "scissors"]
+
+// Take an input from the user
+// Ensure the input is either rock, paper, or scissors, case insensitive
+
+while (playerScore < 3 && computerScore < 3) {
+
+	choiceMade = false
+	let playerChoice = null
+
+	while (choiceMade === false) {
+		playerChoice = prompt("What is your selection? (Rock, paper, scissors)")
+		if (choices.includes(playerChoice.toLowerCase())) {
+			choiceMade = true
+		} else {
+			alert("Invalid input.  Please type rock, paper, or scissors.")
+		}
+	}
+
+	// Convert the choice to lowercase to compare and tell the user what they picked.
+
+	playerChoice = playerChoice.toLowerCase()
+
+	// Make the computer pick a choice between rock, paper and scissors at random.
+
+	let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+	
+	// Compare the choices, and decide if the user or computer wins, and add one to the score of the winner
+
+	compareResults(playerChoice, computerChoice)
+
+}
+
+
+alert(`Final score is User: ${playerScore} - Computer: ${computerScore}`)
+
+if (playerScore > computerScore) {
+	alert(`You won!  Congratulations!`)
+} else {
+	alert(`You lost!  Better luck next time.`)
+}
--- script.js
+++ script.js
@@ -1,93 +1,137 @@
-function showScore () {
-	alert(`Current score - User: ${playerScore}.  Computer: ${computerScore}`)
+function randomChoice () {
+	computerChoice = choices[Math.floor(Math.random() * choices.length)]
 }
 
-function compareResults (user, computer) {
-	
-	if (user === computer) {
-		alert(`You threw ${user} and the computer threw ${computer}.  Tie!`)
-		showScore()
+function updateScore () {
+	const pScoreDisplay = document.querySelector("#player-score-num")
+	const cScoreDisplay = document.querySelector("#computer-score-num")
+	pScoreDisplay.textContent = playerScore
+	cScoreDisplay.textContent = computerScore
+}
 
-	} else if (user === 'rock') {
-		if (computer === 'scissors') {
-			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
-			playerScore ++
-			showScore()
-		} else {
-			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
-			computerScore ++
-			showScore()
-		}
+function playGame (player, computer) {
+	const playText = document.querySelector("#play-text")
+
+	if (player === computer) {
+		playText.textContent = `You both picked ${player}.  Tie!`
 	
-	} else if (user === 'paper') {
-		if (computer === 'rock') {
-			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+	} else if (playerChoice === 'rock') {
+		if (computerChoice === 'scissors') {
 			playerScore ++
-			showScore()
+			updateScore()
+			playText.textContent = `Rock vs Scissors.  You win a point!`
 		} else {
-			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
 			computerScore ++
-			showScore()
+			updateScore()
+			playText.textContent = `Rock vs Paper.  Computer wins a point.`
 		}
 
-	} else if (user === 'scissors') {
-		if (computer === 'paper') {
-			alert(`You threw ${user} and the computer threw ${computer}.  You win!`)
+	} else if (playerChoice === 'paper') {
+		if (computerChoice === 'rock') {
 			playerScore ++
-			showScore()
+			updateScore()
+			playText.textContent = `Paper vs Rock.  You win a point!`
 		} else {
-			alert(`You threw ${user} and the computer threw ${computer}.  You lost!`)
 			computerScore ++
-			showScore()
+			updateScore()
+			playText.textContent = `Paper vs Scissors.  Computer wins a point.`
+		}
+	} else if (playerChoice === 'scissors') {
+		if (computerChoice === 'paper') {
+			playerScore ++
+			updateScore()
+			playText.textContent = `Scissors vs Paper.  You win a point!`
+		} else {
+			computerScore ++
+			updateScore()
+			playText.textContent = `Scissors vs Rock.  Computer wins a point.`
 		}
 	}
 }
 
+
+
+
 // Set the scores for player and computer to 0
 
 let playerScore = 0
+let playerChoice = null
+
 let computerScore = 0
+let computerChoice = null
 
 // Create an array of the acceptable choices.
 
 const choices = ["rock", "paper", "scissors"]
 
+const rock = document.querySelector("#rock-select")
+rock.addEventListener('click', () => {
+	playerChoice = "rock"
+	randomChoice()
+	playGame(playerChoice, computerChoice)
+})
+
+const paper = document.querySelector("#paper-select")
+paper.addEventListener('click', () => {
+	playerChoice = "paper"
+	randomChoice()
+	playGame(playerChoice, computerChoice)
+})
+
+const scissors = document.querySelector("#scissors-select")
+scissors.addEventListener('click', () => {
+	playerChoice = "scissors"
+	randomChoice()
+	playGame(playerChoice, computerChoice)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 // Take an input from the user
 // Ensure the input is either rock, paper, or scissors, case insensitive
 
-while (playerScore < 3 && computerScore < 3) {
+// while (playerScore < 3 && computerScore < 3) {
 
-	choiceMade = false
-	let playerChoice = null
+// 	choiceMade = false
+// 	let playerChoice = null
 
-	while (choiceMade === false) {
-		playerChoice = prompt("What is your selection? (Rock, paper, scissors)")
-		if (choices.includes(playerChoice.toLowerCase())) {
-			choiceMade = true
-		} else {
-			alert("Invalid input.  Please type rock, paper, or scissors.")
-		}
-	}
+// 	while (choiceMade === false) {
+// 		playerChoice = prompt("What is your selection? (Rock, paper, scissors)")
+// 		if (choices.includes(playerChoice.toLowerCase())) {
+// 			choiceMade = true
+// 		} else {
+// 			alert("Invalid input.  Please type rock, paper, or scissors.")
+// 		}
+// 	}
 
-	// Convert the choice to lowercase to compare and tell the user what they picked.
+// 	// Convert the choice to lowercase to compare and tell the user what they picked.
 
-	playerChoice = playerChoice.toLowerCase()
+// 	playerChoice = playerChoice.toLowerCase()
 
-	// Make the computer pick a choice between rock, paper and scissors at random.
+// 	// Make the computer pick a choice between rock, paper and scissors at random.
 
-	let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+// 	let computerChoice = choices[Math.floor(Math.random() * choices.length)]
 	
-	// Compare the choices, and decide if the user or computer wins, and add one to the score of the winner
+// 	// Compare the choices, and decide if the user or computer wins, and add one to the score of the winner
 
-	compareResults(playerChoice, computerChoice)
+// 	compareResults(playerChoice, computerChoice)
 
-}
+// }
 
 
-alert(`Final score is User: ${playerScore} - Computer: ${computerScore}`)
+// alert(`Final score is User: ${playerScore} - Computer: ${computerScore}`)
 
-if (playerScore > computerScore) {
-	alert(`You won!  Congratulations!`)
-} else {
-	alert(`You lost!  Better luck next time.`)
-}
+// if (playerScore > computerScore) {
+// 	alert(`You won!  Congratulations!`)
+// } else {
+// 	alert(`You lost!  Better luck next time.`)
+// }
